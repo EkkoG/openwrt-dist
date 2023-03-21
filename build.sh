144a8432a2ba9d84f98004c8f86e7cf722780161
EkkoG <beijiu572@gmail.com>
2023-03-21 16:26:06 +0800

    Add v2dat, first build x86_64, try mips

--- build.sh
+++ build.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+# mkdir openwrt-packages
 
-sudo apt-get update
-sudo apt-get install upx -y
-cp /usr/bin/upx staging_dir/host/bin
-cp /usr/bin/upx-ucl staging_dir/host/bin
+# git clone --depth=1 --branch v5-dev https://github.com/sbwml/luci-app-mosdns.git sbwml-packages
+# cp -r sbwml-packages/luci-app-mosdns openwrt-packages/luci-app-mosdns
+# rm -r sbwml-packages
 
-./scripts/feeds update -a
+# git clone --depth=1 https://github.com/immortalwrt/packages.git immortal-packages
+# cp -r immortal-packages/net/mosdns openwrt-packages/mosdns
+# cp -r immortal-packages/net/v2ray-geodata openwrt-packages/v2ray-geodata
+# sed -i '/golang-package.mk/ c\include $(TOPDIR)/feeds/packages/lang/golang/golang-package.mk' openwrt-packages/mosdns/Makefile
+# rm -r immortal-packages
+
+# git clone --depth=1 https://github.com/immortalwrt/luci.git immortal-luci
+# cp -r immortal-luci/applications/luci-app-openclash openwrt-packages/luci-app-openclash
+# rm -r immortal-luci
+
+# git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git openwrt-packages/luci-theme-argon
+# git clone --depth=1 https://github.com/gSpotx2f/luci-app-temp-status.git openwrt-packages/luci-app-temp-status
+# git clone --depth=1 https://github.com/EkkoG/sdm.git openwrt-packages/sdm
+# git clone --depth=1 https://github.com/EkkoG/clash-meta-for-openclash.git openwrt-packages/clash-meta-for-openclash
 
 
-OPENWRT_VERSION=$(sed -n 's/.*releases\/\(.*\)\/targets.*/\1/p' <<< $SDK_URL)
+# wget https://testingcf.jsdelivr.net/gh/alecthw/mmdb_china_ip_list@release/lite/Country.mmdb -O openwrt-packages/luci-app-openclash/root/etc/openclash/Country.mmdb
+# wget https://testingcf.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -O openwrt-packages/luci-app-openclash/root/etc/openclash/GeoSite.dat
+# wget https://testingcf.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -O openwrt-packages/luci-app-openclash/root/etc/openclash/GeoIP.dat
 
-if [[ $OPENWRT_VERSION == 19* ]];then
-    pushd feeds/packages/lang
-    rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-21.02/lang/golang
-    popd
-fi
-
-./scripts/feeds install luci-app-passwall
-
+# cp feeds.conf.default feeds.conf
+# cp bin/feeds.conf feeds.conf
+# echo "src-link openwrtpackages $(pwd)/openwrt-packages" >> ./feeds.conf
+# ./scripts/feeds update -a
 make defconfig
 
-make package/luci-app-passwall/{clean,compile} -j4
-make package/index
+pkgs="luci-app-mosdns luci-app-openclash luci-theme-argon luci-app-temp-status sdm clash-meta-for-openclash"
+for pkg in $pkgs; do
+    ./scripts/feeds install -p openwrtpackages -f $pkg
+    make package/$pkg/download V=s
+    make package/$pkg/check V=s
+    make package/$pkg/compile V=s
+done
