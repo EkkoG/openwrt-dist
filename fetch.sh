2df3bf2e4a0dd6d3d35c3877a600bde6162341cb
EkkoG <beijiu572@gmail.com>
2023-03-21 23:55:54 +0800

    Split fetch to script

---
+++ fetch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+get_latest_release() {
+    url=https://api.github.com/repos/$1/releases?per_page=1
+    curl $url | grep tag_name | cut -d '"' -f 4 | tr -d 'v'
+}
+mkdir openwrt-packages
+
+git clone --depth=1 --branch v5-dev https://github.com/sbwml/luci-app-mosdns.git sbwml-packages
+cp -r sbwml-packages/luci-app-mosdns openwrt-packages/luci-app-mosdns
+cp -r sbwml-packages/v2dat openwrt-packages/v2dat
+rm -rf sbwml-packages
+
+git clone --depth=1 https://github.com/immortalwrt/packages.git immortal-packages
+cp -r immortal-packages/net/mosdns openwrt-packages/mosdns
+cp -r immortal-packages/net/v2ray-geodata openwrt-packages/v2ray-geodata
+sed -i '/golang-package.mk/ c\include $(TOPDIR)/feeds/packages/lang/golang/golang-package.mk' openwrt-packages/mosdns/Makefile
+rm -rf immortal-packages
+
+openclash_version=$(get_latest_release "vernesong/OpenClash")
+git clone --depth=1 --branch v$openclash_version https://github.com/vernesong/OpenClash.git
+cp -r OpenClash/luci-app-openclash openwrt-packages/luci-app-openclash
+rm -rf OpenClash
+
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git openwrt-packages/luci-theme-argon
+git clone --depth=1 https://github.com/gSpotx2f/luci-app-temp-status.git openwrt-packages/luci-app-temp-status
+git clone --depth=1 https://github.com/EkkoG/sdm.git openwrt-packages/sdm
+git clone --depth=1 https://github.com/EkkoG/clash-meta-for-openclash.git openwrt-packages/clash-meta-for-openclash
+
+
+wget https://testingcf.jsdelivr.net/gh/alecthw/mmdb_china_ip_list@release/lite/Country.mmdb -O openwrt-packages/luci-app-openclash/root/etc/openclash/Country.mmdb
+wget https://testingcf.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -O openwrt-packages/luci-app-openclash/root/etc/openclash/GeoSite.dat
+wget https://testingcf.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -O openwrt-packages/luci-app-openclash/root/etc/openclash/GeoIP.dat
