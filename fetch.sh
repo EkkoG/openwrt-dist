3756b19ae752a7c9caf77f924ca62256ee2f16de
EkkoG <beijiu572@gmail.com>
2023-03-28 14:43:24 +0800

    Try

--- fetch.sh
+++ fetch.sh
@@ -42,3 +42,10 @@ rm -rf official-packages
 wget https://testingcf.jsdelivr.net/gh/alecthw/mmdb_china_ip_list@release/lite/Country.mmdb -O openwrt-packages/luci-app-openclash/root/etc/openclash/Country.mmdb
 wget https://testingcf.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -O openwrt-packages/luci-app-openclash/root/etc/openclash/GeoSite.dat
 wget https://testingcf.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -O openwrt-packages/luci-app-openclash/root/etc/openclash/GeoIP.dat
+
+BRANCH="packages/${ARCH::-2}"
+if [[ $ARCH =~ "snapshot" ]]; then
+    BRANCH="packages/$ARCH"
+fi
+
+echo "Will push to $BRANCH"
