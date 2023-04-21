b828776674030f0ef6365df9754fc02de54a0e3d
EkkoG <beijiu572@gmail.com>
2023-04-21 22:54:21 +0800

    Clean

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-./fetch.sh
+# ./fetch.sh
 
-cp feeds.conf.default feeds.conf
-echo "src-link openwrtpackages $(pwd)/openwrt-packages" >> ./feeds.conf
-./scripts/feeds update -a
-make defconfig
-
-pkgs="natmap"
-for pkg in $pkgs; do
-    ./scripts/feeds install -p openwrtpackages -f $pkg
-    make package/$pkg/download V=s
-    make package/$pkg/check V=s
-    make package/$pkg/compile V=s
-done
+# cp feeds.conf.default feeds.conf
+# echo "src-link openwrtpackages $(pwd)/openwrt-packages" >> ./feeds.conf
+# ./scripts/feeds update -a
+# make defconfig
+sudo apt update
+sudo apt install shfmt -y
+# pkgs="mosdns"
+# for pkg in $pkgs; do
+#     ./scripts/feeds install -p openwrtpackages -f $pkg
+#     make package/$pkg/download V=s
+#     make package/$pkg/check V=s
+#     make package/$pkg/compile V=s
+# done
--- sdm/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "src-link ekko /home/build/openwrt/ekko" >> ./feeds.conf
-./scripts/feeds update ekko
-make defconfig
-./scripts/feeds install -a -p ekko
-make package/sdm/compile V=s
