d313b8590855ed6427a4a6a151847c780abb7d5a
EkkoG <beijiu572@gmail.com>
2023-09-12 16:03:59 +0800

    Reimplement add feed

--- add-feed.sh
+++ add-feed.sh
@@ -1,36 +1,62 @@
 #!/bin/sh -e
 
+feed=$1
+
 . /etc/openwrt_release 
-DISTRIB_RELEASE=$(echo $DISTRIB_RELEASE | awk -F. '{print $1"."$2}')
-if [ "$DISTRIB_RELEASE" = "snapshot" ]; then
-    DISTRIB_RELEASE="SNAPSHOT"
-elif [ "$ISTRIB_RELEASE" != "21.02" ] && [ "$DISTRIB_RELEASE" != "22.03" ]; then
-    DISTRIB_RELEASE="21.02"
-fi
 
-if grep -q "ekkog" /etc/opkg/customfeeds.conf; then
-    echo "Feed already added"
+remove_old() {
+    if grep -q "ekkog_$feed" /etc/opkg/customfeeds.conf; then
+        echo "Old feed already exists, remove it..."
+        sed -i "/ekkog_$feed/d" /etc/opkg/customfeeds.conf
+    fi
+}
 
-    echo "Remove feed..."
-    sed -i '/ekkog/d' /etc/opkg/customfeeds.conf
-fi
+add_key() {
+    if [ -f /etc/opkg/keys/cd5844109a8e9dda ]; then
+        echo "Key already added, skip"
+    else
+        echo "Add key..."
+        wget https://github.com/ekkog/openwrt-dist/raw/master/cd5844109a8e9dda
+        opkg-key add cd5844109a8e9dda
+        rm cd5844109a8e9dda
+    fi
+}
 
-feed="
-src/gz ekkog_packages https://github.com/ekkog/openwrt-packages/raw/${DISTRIB_ARCH}-${DISTRIB_RELEASE}
-src/gz ekkog_luci https://github.com/ekkog/openwrt-luci/raw/${DISTRIB_RELEASE}
-"
-echo "Add feed..."
-echo "$feed" >> /etc/opkg/customfeeds.conf
 
-if [ -f /etc/opkg/keys/cd5844109a8e9dda ]; then
-    echo "Key already added"
+all_supported=$(curl https://sourceforge.net/projects/ekko-openwrt-dist/files/$feed/ | grep -e "<th.*files/$feed" | grep -o 'href="/projects[^"]*"' | sed 's/href="//' | sed 's/"$//' | awk -F/ '{print $6}')
+echo "All supported version: "
+echo "$all_supported"
+
+branch=$(echo "$DISTRIB_RELEASE" | awk -F- '{print $1}')
+supported=$(echo "$all_supported" | grep $DISTRIB_ARCH | grep $branch)
+
+if [ -z "$supported" ]; then
+    echo "Your device is not supported"
 else
-    echo "Add key..."
-    wget https://github.com/ekkog/openwrt-dist/raw/master/cd5844109a8e9dda
-    opkg-key add cd5844109a8e9dda
-    rm cd5844109a8e9dda
+    feed_version="$DISTRIB_ARCH-v$DISTRIB_RELEASE"
+    full_support=0
+    for i in $supported; do
+        if [ "$i" = "$DISTRIB_ARCH-v$DISTRIB_RELEASE" ]; then
+            full_support=1
+            break
+        fi
+    done
+
+    if [ "$full_support" = "0" ]; then
+        echo "Your device is not fully supported"
+        echo "Find the latest version that supports your device"
+
+        final_release=$(echo "$supported" | grep -v "\-rc" | grep -v "\-SNAPSHOT")
+        if [ -z "$final_release" ]; then
+            echo "No final release found, use the latest rc version"
+            feed_version=$(echo "$supported" | grep "\-rc" | tail -n 1)
+        else
+            feed_version=$final_release
+        fi
+    fi
+    echo "Feed version: $feed_version"
+    remove_old
+    echo "src/gz ekkog_$feed https://ghproxy.imciel.com/https://downloads.sourceforge.net/project/ekko-openwrt-dist/clash/$feed_version" >> /etc/opkg/customfeeds.conf
+    add_key
 fi
 
-echo "Update feeds..."
-
-opkg update
